725a55bab2d0eb9f8067a22817e1d51da156af9d
ChenYenTang <39148857+ChenYenTang@users.noreply.github.com>
2025-08-15 01:49:32 +0800

    把 Gradio 掛到 FastAPI 的 /ui

---
+++ src/main.py
@@ -0,0 +1,12 @@
+
+import gradio as gr
+from fastapi import FastAPI
+from src.api.routes import app as api_app
+from src.ui.gradio_app import build_demo
+
+# Build Gradio UI
+demo = build_demo()
+
+# Mount on FastAPI at /ui
+app: FastAPI = api_app
+app = gr.mount_gradio_app(app, demo, path="/ui")
